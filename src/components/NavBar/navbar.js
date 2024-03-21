aded026d4e8976446723aa3a697a1efaebecbb88
nikhitag79 <nguntu@gmail.com>
2024-03-21 15:04:47 -0400

    added navbar to scroll

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -9,20 +9,26 @@ const Navbar = () => {
     <nav className='navbar'>
     <img src={logo} alt="logo" className='logo' />
         <div className="desktopMenu">
-            <Link className='desktopMenuListItem'>Home </Link>
-            <Link className='desktopMenuListItem'> About</Link>
-            <Link className='desktopMenuListItem'> Portfolio</Link>
-            <Link className='desktopMenuListItem'> Clients</Link>
+            <Link activateClass = 'active' to ='intro' spy={true} smoth = {true} offset = {-100} duration = {500} className='desktopMenuListItem'>Home </Link>
+            <Link activateClass = 'active' to ='skills' spy={true} smoth = {true} offset = {-70} duration = {500}  Link className='desktopMenuListItem'> About</Link>
+            <Link activateClass = 'active' to ='works' spy={true} smoth = {true} offset = {-50} duration = {500} Link className='desktopMenuListItem'> Portfolio</Link>
+            <Link activateClass = 'active' to ='clients' spy={true} smoth = {true} offset = {-50} duration = {500} Link className='desktopMenuListItem'> Clients</Link>
 
 
 
 
         </div>
-        <button className='desktopMenuBtn'>
+        <button className='desktopMenuBtn' onClick={() => {
+          document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
+        }
+        
+        }>
             <img src={contactImg} alt="" className="desktopMenuImg"></img>
             Contact me
 
         </button>
+
+        
 
 
     </nav>
