00c7a5819492e785f3e133c0217ace65d77177bb
nikhitag79 <nguntu@gmail.com>
2024-03-24 04:57:56 -0400

    changed webdev to 5.3.4

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,11 +1,32 @@
-import React from 'react';
 import github from '../../assets/github.png';
 import linkedin from '../../assets/linkedin.png';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 import './contact.css'
 
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('service_e386g9m', 'template_avk82h8', form.current, {
+            publicKey: 'H8eQlmJujvzU1IZHs',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+              e.target.reset();
+              alert("Email Sent!")
+             },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
+
     return(
     <section id="contactPage" >
         <div id = "clients">
@@ -16,10 +37,10 @@ const Contact = () => {
             <span className="contactDes">
                 Please fill out the form for further inquiries
             </span>
-            <form className='contactForm'>
-                <input type ="text" className="name" placeholder ="Your Name"/>
-                <input type ="email" className="email" placeholder ="Your Email"/>
-                <textarea className="msg" rows="5" placeholder="Your Message"></textarea>
+            <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                <input type ="text" className="name" placeholder ="Your Name" name = "from_name"/>
+                <input type ="email" className="email" placeholder ="Your Email" name = "from_email"/>
+                <textarea className="msg" rows="5" placeholder="Your Message" name = "message"></textarea>
                 <button type="submit" value="Send" className="submitBtn">Submit</button>
                 <div className="links">
                     <img src={github} alt="" className="link" />
